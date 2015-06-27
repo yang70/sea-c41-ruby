ec082539ab8021ccf17d0751e7e65f8f4de3eb5e
Matthew Yang <yang70@gmail.com>
2015-06-27 09:59:39 -0700

    Submit class 2 assignments

--- lib/class2/exercise7.rb
+++ lib/class2/exercise7.rb
@@ -29,10 +29,26 @@
 #     Returns a new String of length Integer with `string` right justified.
 #
 #     'bird'.rjust(10)  #=> '      bird'
-puts 'Table of Contents'.center(50)
-print 'Chapter 1:  Getting Started'
-puts 'page  1'.rjust(50 - 27)
-print 'Chapter 2:  Numbers'
-puts 'page  9'.rjust(50 - 19)
-print 'Chapter 3:  Letters'
-puts 'page 13'.rjust(50 - 19)
+
+# create the hash of strings
+table_of_contents = {
+  'Table of Contents' => nil,
+  'Chapter 1:  Getting Started' => 'page  1',
+  'Chapter 2:  Numbers' => 'page  9',
+  'Chapter 3:  Letters' => 'page 13'
+}
+
+# define the function to print and format the strings for the given line width
+def print_toc(table_hash, line_width)
+  table_hash.each do |key, value|
+    if value.nil?
+      puts key.center(line_width)
+    else
+      print key.ljust(key.length)
+      puts value.rjust(line_width - key.length)
+    end
+  end
+end
+
+# call the variable and pass it the hash and line width
+print_toc(table_of_contents, 50)
