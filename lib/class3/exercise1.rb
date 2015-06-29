c3b75d151a555c8bd5f062265de253544c9bc94d
Matthew Yang <yang70@gmail.com>
2015-06-29 08:05:19 -0700

    complete class3 exercise1

--- lib/class3/exercise1.rb
+++ lib/class3/exercise1.rb
@@ -27,10 +27,9 @@ while bottles > 1
     puts 'bottles of beer on the wall!'
   else
     puts 'Take one down, pass it around, 1 bottle of beer on the wall!'
+    puts '1 bottle of beer on the wall, 1 bottle of beer!'
+    puts 'Take one down, pass it around, no more bottles of beer on the wall!'
+    puts 'No more bottles of beer on the wall, no more bottles of beer!'
+    puts 'Go to the store and buy some more, 3 bottles of beer on the wall!'
   end
 end
-
-puts '1 bottle of beer on the wall, 1 bottle of beer!'
-puts 'Take one down, pass it around, no more bottles of beer on the wall!'
-puts 'No more bottles of beer on the wall, no more bottles of beer!'
-puts 'Go to the store and buy some more, 3 bottles of beer on the wall!'
