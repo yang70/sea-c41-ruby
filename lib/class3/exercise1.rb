5f8bbc59d8b6d29dd38b5ef3363f465c3b1e1986
Matthew Yang <yang70@gmail.com>
2015-06-29 07:41:53 -0700

    complete class3 exercise1

--- lib/class3/exercise1.rb
+++ lib/class3/exercise1.rb
@@ -16,3 +16,21 @@
 #   Go to the store and buy some more, 3 bottles of beer on the wall!
 #
 # TIP: I expect you to use a loop.
+
+bottles = 3
+
+while bottles > 1
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer!"
+  bottles -= 1
+  if bottles > 1
+    print "Take one down, pass it around, #{bottles} "
+    puts 'bottles of beer on the wall!'
+  else
+    puts 'Take one down, pass it around, 1 bottle of beer on the wall!'
+  end
+end
+
+puts '1 bottle of beer on the wall, 1 bottle of beer!'
+puts 'Take one down, pass it around, no more bottles of beer on the wall!'
+puts 'No more bottles of beer on the wall, no more bottles of beer!'
+puts 'Go to the store and buy some more, 3 bottles of beer on the wall!'
