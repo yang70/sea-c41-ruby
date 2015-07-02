efc9d723fbf1c97b2d6413ca59b843218c8448d5
Matthew Yang <yang70@gmail.com>
2015-07-02 13:20:14 -0700

    complete class4 exercise5

--- lib/class4/exercise5.rb
+++ lib/class4/exercise5.rb
@@ -33,7 +33,32 @@
 
 # rubocop:disable MethodLength
 def modern_roman_numeral(num)
-  num # change me
+  roman = ''
+  roman += 'M' * (num / 1000)
+  num = num % 1000
+  roman += 'CM' * (num / 900)
+  num = num % 900
+  roman += 'D' * (num / 500)
+  num = num % 500
+  roman += 'CD' * (num / 400)
+  num = num % 400
+  roman += 'C' * (num / 100)
+  num = num % 100
+  roman += 'XC' * (num / 90)
+  num = num % 90
+  roman += 'L' * (num / 50)
+  num = num % 50
+  roman += 'XL' * (num / 40)
+  num = num % 40
+  roman += 'X' * (num / 10)
+  num = num % 10
+  roman += 'IX' * (num / 9)
+  num = num % 9
+  roman += 'V' * (num / 5)
+  num = num % 5
+  roman += 'IV' * (num / 4)
+  num = num % 4
+  roman + 'I' * num
 end
 
 input = ARGV[0].to_i
